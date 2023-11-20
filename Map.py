b9bb625317b6c438f787e7c01e19b85f95af49a8
Por-Tra <151206590+Por-Tra@users.noreply.github.com>
2023-11-20 21:08:15 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -4,6 +4,17 @@ import pytmx
 import pyscroll
 
 
+
+@dataclass
+class Portal:
+
+    from_world: str  #quel monde
+    origin_point: str    #sur quel point
+    target_world: str    #sur quel monde
+    teleport_point: str   #point de téléporation
+
+
+
 @dataclass    #équivalent du __init__
 class Map:
 
@@ -11,23 +22,42 @@ class Map:
     walls: list[pygame.Rect]
     group: pyscroll.PyscrollGroup
     tmx_data: pytmx.TiledMap
+    portals: list[Portal]
+    #npc: list[NPC]
 
 class MapManager:
 
-    def __int__(self,ecran,joueur):
-        self.maps = dict()   #'house' -> Map("house, walls,group)
+    def __init__(self, ecran, joueur):
+        self.maps = dict()              #'house' -> Map("house, walls,group)
         self.screen = ecran
         self.player = joueur
-        self.current_map = 'map'    #carte de début
+        self.current_map = 'map'        #carte de début
 
-        self.register_map("map")
-        self.register_map("cave")
+        self.register_map("map" , portals=[
+            Portal(from_world='world' , origin_point='enter_cave',target_world='cave',teleport_point='spawn_cave')
+        ])
+        self.register_map("cave", portals=[
+            Portal(from_world='cave',origin_point='exit_cave',target_world='world',teleport_point='exit_cave_spawn')
+        ])
 
         self.teleport_player('spawn')
 
     def check_collisions(self):
+
+        #portail
+        for portal in self.get_map().portals:
+            if portal.from_world == self.current_map:
+                point = self.get_object(portal.origin_point)
+                rect = pygame.Rect(point.x,point.y,point.widht,point.height)
+
+                if self.player.feet.colliderect(rect):
+                    copy_portal = portal
+                    self.current_map = portal.target_world
+                    self.teleport_player(copy_portal.teleport_point)
+
+        #collision
         for sprite in self.get_group().sprites():
-            if sprite.feeet.collidelist(self.get_walls()) > -1:
+            if sprite.feet.collidelist(self.get_walls()) > -1:
                 sprite.move_back()
 
     def teleport_player(self,name_point):
@@ -39,10 +69,10 @@ class MapManager:
         point = self.get_object(name_point)
         self.player.position[0] = point.x
         self.player.position[1] = point.y
-        self.player.save_location() #pour éviter les bug de téléportation avec les collisions
+        self.player.save_location()      #pour éviter les bug de téléportation avec les collisions
 
 
-    def register_map(self,name):
+    def register_map(self,name,portals=[]):
         """
         enregistrer les maps
         :param name:
@@ -61,11 +91,11 @@ class MapManager:
                 walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
 
         # Dessiner les différents calques
-        group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=5)
+        group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=6)
         group.add(self.player)
 
         #cree objet map
-        self.maps[name] = Map(name,walls,group,tmx_data)
+        self.maps[name] = Map(name,walls,group,tmx_data, portals)
 
     #récupérer la map
     def get_map(self):return self.maps[self.current_map]

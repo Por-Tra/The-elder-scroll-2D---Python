e98777e4cedc99698904025047b71f8ba88e4d55
Por-Tra <151206590+Por-Tra@users.noreply.github.com>
2023-11-29 18:27:15 +0100

    Delete assets/map/tuiles_grotte.tsx

--- assets/map/tuiles_grotte.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tuiles grotte " tilewidth="32" tileheight="32" tilecount="2448" columns="51">
- <image source="MainLev2.0.png" trans="000000" width="1632" height="1536"/>
-</tileset>
